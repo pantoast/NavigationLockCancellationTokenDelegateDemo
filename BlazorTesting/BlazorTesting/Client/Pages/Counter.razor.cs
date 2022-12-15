6c310c11cda252372cb25baaefde59ce700bcfa8
Adam Klein <56697785+pantoast@users.noreply.github.com>
2022-12-15 16:35:29 -0500

    Initial upload

---
+++ BlazorTesting/BlazorTesting/Client/Pages/Counter.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+
+namespace BlazorTesting.Client.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+
+        public void OnBeforeInternalNavigation(LocationChangingContext context)
+        {
+            context.CancellationToken.Register(() =>
+            {
+                Console.WriteLine("NAV CANCELED");
+            });
+        }
+    }
+}
